0a18121335a7d515e4255d72867df44c79ba8611
Liam Eller <liamthelastson@gmail.com>
2021-11-19 12:20:02 -0800

    WIP: off to lunch

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,14 +1,19 @@
 export default class APIService {
-  static async APIrequest(){
+  static async APIRequest(USD){
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${procces.env.API_KEY}/latest/USD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      console.log(response.ok);
       if (!response.ok) {
+        console.log(response.ok);
+        console.log(Error(response.statusText));
         throw Error(response.statusText);
       }
-      return await response.JSON();
+      console.log(response.result);
+      console.log(response);
+      return await response.json();
     }catch(error) {
       console.log(error);
       return error.message;
     }
-  };
+  }
 }
